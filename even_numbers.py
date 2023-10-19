e69b6a40a4ab2b6b7304c99dd65322d2dc47f34b
Vasil Tarashev <145684223+waskoooo@users.noreply.github.com>
2023-10-19 09:10:24 +0300

    Add files via upload

--- even_numbers.py
+++ even_numbers.py
@@ -1,7 +1,14 @@
-num_as_string = input().split()
-num_as_digits = []
-for number in num_as_string:
-    num_as_digits.append((int(number)))
-is_even = lambda  x: x % 2 == 0
-result = list(filter(is_even, num_as_digits))
-print(result)
+# Take input from the user
+numbers = input()
+
+# Split the input into a list of strings
+numbers_list = numbers.split(", ")
+
+# Convert the list of strings into a list of integers
+numbers_int = [int(x) for x in numbers_list]
+
+# Find the indices of all even numbers
+even_indices = [i for i, num in enumerate(numbers_int) if num % 2 == 0]
+
+# Print the result
+print(even_indices)
