c7cc2532648a59621786ae064c5de21ff9444af2
Vasil Tarashev <145684223+waskoooo@users.noreply.github.com>
2023-11-02 13:11:06 +0200

    Add files via upload

---
+++ 1_1_storage.py
@@ -0,0 +1,23 @@
+def get_product():
+    return Storage.storage
+
+
+class Storage:
+    storage = []
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+
+    def add_product(self, product: str):
+        if self.capacity > 0:
+            self.capacity -= 1
+            Storage.storage.append(product)
+
+
+storage = Storage(4)
+storage.add_product("apple")
+storage.add_product("banana")
+storage.add_product("potato")
+storage.add_product("tomato")
+storage.add_product("bread")
+print(get_product())
