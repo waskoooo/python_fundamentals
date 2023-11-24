18c5a21982329905f0f473bdddd794cb8944b9cc
Vasil Tarashev <145684223+waskoooo@users.noreply.github.com>
2023-11-24 19:46:22 +0200

    Add files via upload

---
+++ 9_9_rage_quit.py
@@ -0,0 +1,23 @@
+text = input()
+unique_symbols = ""
+repetitions = ""
+current_symbol = ""
+
+for index in range(len(text)):
+    if not text[index].isdigit():  # we have non numeric character
+        current_symbol += text[index].upper()
+    else:   # we have digit here --> time for repetition
+        for next_symbols in range(index, len(text)):
+            if not text[next_symbols].isdigit():
+                break
+            repetitions += text[next_symbols]
+        unique_symbols += current_symbol * int(repetitions)
+        current_symbol = ""
+        repetitions = ""
+
+print(f"Unique symbols used: {len(set(unique_symbols))}")
+print(f"{unique_symbols}")
+
+
+
+
