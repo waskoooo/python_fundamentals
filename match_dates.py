978bd236a78def8ad3d683936284d4a6044f465c
Vasil Tarashev <145684223+waskoooo@users.noreply.github.com>
2023-11-25 11:42:49 +0200

    Add files via upload

---
+++ match_dates.py
@@ -0,0 +1,13 @@
+import re
+
+string = input()
+
+regex = r"(\d{2})([-./])([A-Z][a-z]{2})\2(\d{4})"
+
+matches = re.findall(regex, string)
+
+for match in matches:
+    day = match[0]
+    month = match[2]
+    year = match[3]
+    print(f'Day: {day}, Month: {month}, Year: {year}')
